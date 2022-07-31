1c21fd97bd231bd6c7b20b5ef1bb5483463c97f2
Juhyeon Lee <gogori6565@naver.com>
2022-07-31 23:55:20 +0900

    [BOJ] C++

--- [BOJ] C++/[BOJ] 2292_벌집.cpp
+++ [BOJ] C++/[BOJ] 2292_벌집.cpp
@@ -5,6 +5,13 @@ int main(void)
 {
     cin.tie(NULL); ios_base::sync_with_stdio(false);
 
-    int n;
+    int n,x=1,sum=1;
     cin>>n;
+
+    while(1){
+        if(sum>=n) break;
+        sum+=6*x;
+        x++;
+    }
+    cout<<x;
 }
