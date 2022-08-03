b96785b85d5937ccc03fadba0995684c973a878c
Juhyeon Lee <gogori6565@naver.com>
2022-08-04 02:56:20 +0900

    [BOJ] 기본수학1_달팽이는올라가고싶다

--- [BOJ] C++/[BOJ] 2869_달팽이는올라가고싶다.cpp
+++ [BOJ] C++/[BOJ] 2869_달팽이는올라가고싶다.cpp
@@ -11,3 +11,15 @@ int main(void)
 
     cout<<int(ceil((double)(v-a)/(double)(a-b)))+1;
 }
+
+/* 입력 자료형을 double로 받아도 가능
+int main(void)
+{
+    cin.tie(NULL); ios_base::sync_with_stdio(false);
+
+    double a,b,v;
+    cin>>a>>b>>v;
+
+    cout<<int(ceil((v-a)/(a-b)))+1;
+}
+*/
